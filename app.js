787121ba005dafc01c34cfc997256cd4bd8ce05d
NullCodex <jamesonyu95@gmail.com>
2017-10-25 14:25:43 -0400

    created application to crawl riot apis

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // accountId = 40831277
 
 // exploer(40831277, 3)
@@ -16,41 +15,83 @@
 
 // todo: random function to get an account id from a selected league
 
-var url = 'mongodb://localhost:27017/test';
-var request = requir('request');
+var url = 'mongodb://localhost:27017/timewinder';
+var request = require('request');
 var accountId = 40831277;
 var MongoClient = require('mongodb').MongoClient;
-var apiKey = process.env.key;
+var apiKey = process.env.RIOT_KEY;
 var baseUrl = "https://na1.api.riotgames.com";
 var keyParam = "?api_key=" + apiKey;
-var ObjectId = require('mongodb').ObjectID;
+var Long = require('mongodb').Long;
 
 MongoClient.connect(url, function(err, db) {
-  explor(accountId, 4, db);
+    if (err) {
+        console.log(err);
+    } else {
+        explor(accountId, 2, db);
+    }
 });
 
-
 function explor(accountId, level, db) {
-  if (level === 0) return;
-
-  request(baseUrl + "/lol/match/v3/matchlists/by-account/" + accountId + keyParam, function (err, res, body) {
-    var matches = body.matches;
-
-    for (match in matches) {
-      var gameId = match.gameId;
-      request(baseUrl + "/lol/match/v3/matches/" + gameId + keyParam, function(err, res, body) {
-        db.collection('matches').find({"_id": ObjectId(gameId)}, function (err, match) {
-          if (match == null) {
-            db.collection('matches').insert({_id: ObjectId(gameId), match: body}, function(err, result) {
-              for (partipant in body.participantIdentities) {
+    console.log(accountId);
+    if (level === 0) return;
+    request({
+        url: baseUrl + "/lol/match/v3/matchlists/by-account/" + accountId + keyParam,
+        json: true
+    }, function(err, res, matchlistBody) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (res.statusCode == 429) {
                 setTimeout(function() {
-                  explor(partipant.player.accountId, level-1);
-                }, 2000);
-              }
-            });
-          }
-        });
-      });
-    }
-  });
+                    explor(accountId, level, db);
+                }, 120000);
+            } else {
+                var matches = matchlistBody.matches;
+                for (var i = 0; i < matches.length; i++) {
+                    var match = matches[i];
+                    var _gameId = match.gameId;
+
+                    (function(gameId) {
+                        request({
+                            url: baseUrl + "/lol/match/v3/matches/" + gameId + keyParam,
+                            json: true
+                        }, function(err, res, matchDetailBody) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                if (res.statusCode == 429) {
+                                    setTimeout(function() {
+                                        explor(accountId, level, db);
+                                    }, 120000);
+                                } else {
+                                    db.collection('matches').findOne({ "matchId": gameId }, function(err, match) {
+                                        if (err) {
+                                            console.log(err);
+                                        } else {
+                                            if (match == null) {
+                                                db.collection('matches').insert({ matchId: Long(gameId), match: matchDetailBody }, function(err, result) {
+                                                    if (err) {
+                                                        console.log(err);
+                                                    }
+                                                    for (var j = 0; j < matchDetailBody.participantIdentities.length; j++) {
+                                                        var _accountIdParam = matchDetailBody.participantIdentities[j].player.accountId;
+                                                        (function(accountIdParam) {
+                                                            setTimeout(function() {
+                                                                explor(accountIdParam, level - 1);
+                                                            }, 2000);
+                                                        })(_accountIdParam)
+                                                    }
+                                                });
+                                            }
+                                          }
+                                    });
+                                }
+                            }
+                        });
+                    })(_gameId)
+                }
+            }
+        }
+    });
 }
